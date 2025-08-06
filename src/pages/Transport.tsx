1e9ab417d80a2b39a2b1a6c5a658a8f8ab5f87d5
Razee4315 <saqlainrazee@gmail.com>
2025-08-07 00:03:40 +0500

    improve transport image croping

--- src/pages/Transport.tsx
+++ src/pages/Transport.tsx
@@ -75,7 +75,7 @@ const Transport: React.FC = () => {
                 onClick={() => handleVehicleSelect(vehicle)}
               >
                 <div className="vehicle-image">
-                  <img src={vehicle.image} alt={`${vehicle.name}`} style={{ width: '100%', height: 250, objectFit: 'cover', borderRadius: 12 }} />
+                  <img src={vehicle.image} alt={`${vehicle.name}`} style={{ width: '100%', height: 250, objectFit: 'contain', borderRadius: 12, backgroundColor: '#f5f5f5' }} />
                 </div>
                 <div className="vehicle-content">
                   <h3 className="vehicle-name">{vehicle.name}</h3>
@@ -128,7 +128,7 @@ const Transport: React.FC = () => {
                     <img
                       src={selectedVehicle.image}
                       alt={selectedVehicle.name}
-                      style={{ width: '100%', height: 400, objectFit: 'cover', objectPosition: 'center top', borderRadius: 12, cursor: 'zoom-in' }}
+                      style={{ width: '100%', height: 400, objectFit: 'contain', objectPosition: 'center', borderRadius: 12, cursor: 'zoom-in', backgroundColor: '#f5f5f5' }}
                       onClick={() => setPreview({ src: selectedVehicle.image, alt: selectedVehicle.name })}
                     />
                   </div>
